b4b3400fc62618bb47d970a22753d003abc07c08
dyako baram <dyako.baram@gmail.com>
2022-05-18 19:10:36 +0300

    add next-auth

--- fb-clone/pages/index.js
+++ fb-clone/pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import Header from '../components/Header'
 
-export default function Home() {
+export default function Home({session}) {
+  if(!session) return(<Login/>)
   return (
     <div>
       <Head>
@@ -12,4 +13,12 @@ export default function Home() {
       <Header/> 
     </div>
   )
+}
+export async function getServerSideProps(context){
+  const session=await getSession(context);
+  return{
+    props:{
+      session
+    }
+  }
 }
